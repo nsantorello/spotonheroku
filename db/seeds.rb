302bc5ce4735d0900012579b55453ff77b1fb1be
Noah Santorello <nsantorello@gmail.com>
2011-03-10 16:52:42 -0500

    Added seed stuffs...

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,6 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-Question.delete_all
-q1 = Question.create(:text => "What is your name?", :answer_id => 1)
-q2 = Question.create(:text => "What is your name 2?", :answer_id => 5)
-q3 = Question.create(:text => "What is your name 3?", :answer_id => 9)
-
-QuestionResponse.delete_all
-QuestionResponse.create(:text => "Birdie", :question_id => q1.id)
-QuestionResponse.create(:text => "Birdie2", :question_id => q1.id)
-QuestionResponse.create(:text => "Birdie3", :question_id => q1.id)
-a1 = QuestionResponse.create(:text => "Birdie4", :question_id => q1.id)
-a2 = QuestionResponse.create(:text => "Doggy", :question_id => q2.id)
-QuestionResponse.create(:text => "Doggy2", :question_id => q2.id)
-QuestionResponse.create(:text => "Doggy3", :question_id => q2.id)
-QuestionResponse.create(:text => "Doggy4", :question_id => q2.id)
-QuestionResponse.create(:text => "Tweetie", :question_id => q3.id)
-QuestionResponse.create(:text => "Tweetie2", :question_id => q3.id)
-a3 = QuestionResponse.create(:text => "Tweetie3", :question_id => q3.id)
-QuestionResponse.create(:text => "Tweetie4", :question_id => q3.id)
-q1.answer_id = a1.id
-q1.save
-q2.answer_id = a2.id
-q2.save
-q3.answer_id = a3.id
-q3.save
-
 CategoryRelation.delete_all
 CategoryRelation.create(:our_tag => "Games", :fsq_tag => "Arcades")
 CategoryRelation.create(:our_tag => "Games", :fsq_tag => "Bowling Alleys")
@@ -54,7 +29,7 @@ CategoryRelation.create(:our_tag => "Music", :fsq_tag => "Music Venues")
 CategoryRelation.create(:our_tag => "Literature", :fsq_tag => "Bookstores")
 CategoryRelation.create(:our_tag => "Food & Drink", :fsq_tag => "Food and Drink Shops")
 
-load 'import_questions.rb'
+load 'db/import_questions.rb'
 
 #VenueQuestionRelation.delete_all
 #VenueQuestionRelation.create(:venue_id => v1.id, :question_id => q1.id)
