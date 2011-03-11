42c311da7af3e38d91d58beddf7a38f5ce3e49b7
Noah Santorello <nsantorello@gmail.com>
2011-03-11 14:10:36 -0500

    Stupidity abound.

--- app/models/category_relation.rb
+++ app/models/category_relation.rb
@@ -1,7 +1,6 @@
 class CategoryRelation < ActiveRecord::Base
 
 	def CategoryRelation.our_tag_category(fsq_tag)
-		puts fsq_tag.downcase
 		our_topic = CategoryRelation.all.select { |cr| fsq_tag.downcase == cr.fsq_tag.downcase }.first if fsq_tag
 		our_tag = our_topic.our_tag if our_topic
 		if  !our_tag || our_tag == ""
