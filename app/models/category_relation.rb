9165c243dc320b8027962e7e974d906d0f3756e0
Noah Santorello <nsantorello@gmail.com>
2011-03-11 14:07:41 -0500

    More stupidity has been corrected

--- app/models/category_relation.rb
+++ app/models/category_relation.rb
@@ -1,8 +1,10 @@
 class CategoryRelation < ActiveRecord::Base
 
 	def CategoryRelation.our_tag_category(fsq_tag)
-		our_topic = CategoryRelation.all.select { |cr| fsq_tag.downcase == cr.our_tag.downcase }.first if fsq_tag
-		if  !our_topic || our_topic == ""
+		puts fsq_tag.downcase
+		our_topic = CategoryRelation.all.select { |cr| fsq_tag.downcase == cr.fsq_tag.downcase }.first if fsq_tag
+		our_tag = our_topic.our_tag if our_topic
+		if  !our_tag || our_tag == ""
 			"General"
 		else
 			our_topic
