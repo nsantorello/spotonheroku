4b1fc1c50cb33b06167dbeaa8f4841b49faec3cc
Noah Santorello <nsantorello@gmail.com>
2011-03-11 13:19:41 -0500

    iFixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ while (line = file.gets)
 		QuestionResponse.create(:text => r)
 	end
 	
-	question = Question.create(:text => tokens[0].strip, :answer_id => choices[0].id.strip, :tag => tokens[5].strip)
+	question = Question.create(:text => tokens[0].to_s.strip, :answer_id => choices[0].id.to_s.strip, :tag => tokens[5].to_s.strip)
 	
 	choices.each do |c|
 		c.question_id = question.id
@@ -32,7 +32,7 @@ CategoryRelation.delete_all
 
 while (line = file.gets)
 	tokens = line.split(delim)
-	CategoryRelation.create(:fsq_tag => tokens[1].strip, :our_tag => tokens[0].strip)
+	CategoryRelation.create(:fsq_tag => tokens[1].to_s.strip, :our_tag => tokens[0].to_s.strip)
 end
 
 file.close
