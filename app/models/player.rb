182ab310d5fdd16ceff624568091c4f5735b8038
Noah Santorello <nsantorello@gmail.com>
2011-03-11 19:23:50 -0600

    asdf

--- app/models/player.rb
+++ app/models/player.rb
@@ -18,9 +18,9 @@ class Player < ActiveRecord::Base
 			end
 			RoundScore.add_score(self, session, round_score)
 			ScoreHistory.get_today_or_create(self, session.venue).add_score(round_score)
-			round_score >= 5
+			return round_score >= 5
 		else
-			false
+			return false
 		end
 	end
 	
